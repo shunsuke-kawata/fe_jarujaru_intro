21f1d0e79948a991da5b3665bf329e3078bee0e2
shunsuke-kawata <kwt20238120@gmail.com>
2024-02-18 22:13:05 +0900

    init

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000, // ファイルの変更を監視する間隔（ミリ秒）
-      aggregateTimeout: 300, // 変更があったときに再ビルドを開始するまでの遅延（ミリ秒）
-    };
-    return config;
-  },
-};
--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-const Test = () => {
-  return (
-    <>
-      <div>testjiafeoi</div>
-    </>
-  );
-};
-
-export default Test;
